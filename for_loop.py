d63e8ad06c12be6f8745cb9c72edb670f811cf91
[Khulud] <[khuludsaekhan@gmail.com]>
2020-10-10 15:32:44 +0700

    pertemuan 3

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+nama_hewan = ["singa", "macan", "harimau"]
+
+#print(nama_hewan[0])
+#print(nama_hewan[1])
+#print(nama_hewan[2])
+#print(len(nama_hewan))
+#print(nama_hewan)
+
+for hewan in nama_hewan:
+    print(hewan)
+    print("----")
+
+print("done")
---
+++ range.py
@@ -0,0 +1,12 @@
+for i in range(3):
+    print(i)
+
+print("----")
+
+for i in range(2, 20, 3):
+    print(i)
+
+print("----")
+
+for i in range(10):
+    print(i+1)
