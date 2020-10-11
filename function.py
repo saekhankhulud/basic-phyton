ef6074a5c2c65ab722428da81be772d88daaf0c6
[Khulud] <[khuludsaekhan@gmail.com]>
2020-10-11 14:50:09 +0700

    pertemuan 4

--- function.py
+++ function.py
@@ -6,8 +6,8 @@ my_function("Anisya", "Salsabila")
 my_function("Joice")
 
 # function in keywords
-def my_function2 ("child3, child2, child1"):
-    print("The youngest child is" + child3)
+def my_function2 (child3, child2, child1):
+    print("The youngest child is " + child3)
 
-my_function2(child1 = "Regie", child2 = Cyn, child3 = "Meta")
+my_function2(child1 = "Regie", child2 = "Cyn", child3 = "Meta")
 my_function2("Regie", "Cyn", "Meta")
